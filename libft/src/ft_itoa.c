76d1325ddb4fb0bf7f145f158d6bbd98fbef306d
goisetsi <goisetsi@users.noreply.github.com>
2016-05-21 14:53:35 +0200

    FIxed It

--- libft/src/ft_itoa.c
+++ libft/src/ft_itoa.c
@@ -6,13 +6,37 @@
 /*   By: jomeirin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/15 16:37:03 by jomeirin          #+#    #+#             */
-/*   Updated: 2016/05/15 16:37:23 by jomeirin         ###   ########.fr       */
+/*   Updated: 2016/05/15 16:37:23 by goisetsi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int c)
+char	*ft_itoa(int n)
 {
-	return ();
+	char	*str;
+
+	if (n > 2147483647 || n < -2147483647)
+		return (NULL);
+	if (!(str = (char *)malloc(sizeof(char *))))
+		return (NULL);
+	if (n >= 0)
+	{
+		while (n != 0)
+		{
+			*(--str) = '0' + (n % 10);
+			n /= 10;
+		}
+		return (str);
+	}
+	else
+	{
+		while (n != 0)
+		{
+			*(--str) = '0' - (n % 10);
+			n /= 10;
+		}
+		*(--str) = '-';
+	}
+	return (str);
 }
